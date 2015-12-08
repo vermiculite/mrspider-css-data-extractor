a898cd5916d49d76eb065a450b3f590d9a98e875
sean <seanowenhayes@gmail.com>
2015-12-08 07:22:59 +0100

    A bit more complete.

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-module.exports = function () {
+module.exports = function (options) {
+    validateOptions(options);
+    
     return function (page, spider, next) {
-        page.data = {};
+        page.data = page.data || {};
         next();
     };
 };
+
+
+function validateOptions(options) {
+    if(!options) {
+        throw new Error('options are required for a css data extractor');
+    }
+}
--- test/index.js
+++ test/index.js
@@ -3,15 +3,41 @@ var chai = require('chai');
 var should = chai.should();
 var CssDataExtractor = require('..');
 
-describe('cssDataExtractor', function() {
+describe('cssDataExtractor', function () {
 
-    it('should create a data property in the page', function() {
+    var validConfig;
+
+    beforeEach(function () {
+        validConfig = {};
+    });
+
+    it('should create a data property in the page', function () {
         var page = {};
         var spider = {};
         var next = sinon.spy();
-        var cssDataExtractor = CssDataExtractor();
+        var cssDataExtractor = CssDataExtractor(validConfig);
         cssDataExtractor(page, spider, next);
         next.called.should.equal(true);
         should.exist(page.data);
-    })
+    });
+
+    it('should not overwrite an already existing data property given a page with a data property', function () {
+        var page = {
+            data: {
+                success: true
+            }
+        };
+        var spider = {};
+        var next = sinon.spy();
+        var cssDataExtractor = CssDataExtractor(validConfig);
+        cssDataExtractor(page, spider, next);
+        page.data.success.should.equal(true);
+    });
+
+
+    it('should throw an error if passed no options', function () {
+        (function () {
+            var cssDataExtractor = CssDataExtractor(null);
+        }).should.throw(Error);
+    });
 });
